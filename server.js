dd08b483257f9e98e1ec2384a7169dfa4a8fdc2e
pandeyg00121 <pranaypandey00121@gmail.com>
2023-04-07 02:36:28 +0530

    index pages created

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const { application } = require("express");
 const app = express();
 
-app.use("/public", express.static(__dirname + "/public"))
-
+// app.use("/public", express.static(__dirname + "/public"))
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-})
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(1347, () => {
-  console.log("The server is up and running!");
+  console.log("The server is up and running at 1347!");
 });
